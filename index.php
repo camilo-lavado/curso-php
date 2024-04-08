a0bf8c3230960ec779f99cdf9de1914e77033b7e
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2024-04-07 22:14:11 -0400

    a

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php
-echo "Hola Mundo";
+$nombre = "Camilo";
+$edad = 20;
+$altura = 1.75;
+$casado = true;
+?>
+
+//31:39
+
+
+<h1>
+    <?= $nombre
+    ?>
+</h1>
+
+<style>
+    :root {
+        color-scheme: light dark;
+    }
+
+    body {
+        background-color: black;
+        color: blanchedalmond;
+        display: grid;
+        place-content: center;
+    }
+</style>
